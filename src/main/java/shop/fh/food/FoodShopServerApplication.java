a0902ae5687c0597fd81d4590ed0d8b826ea1a4e
Foad <f.h.w.free1988@gmail.com>
2024-03-05 19:06:13 +0330

    first commit

---
+++ src/main/java/shop/fh/food/FoodShopServerApplication.java
@@ -0,0 +1,13 @@
+package shop.fh.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodShopServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodShopServerApplication.class, args);
+    }
+
+}
