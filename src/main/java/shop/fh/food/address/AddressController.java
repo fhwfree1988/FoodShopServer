f9ac485dc13b0fa30fee47666224dae9b51cc566
Foad <f.h.w.free1988@gmail.com>
2024-03-13 16:46:02 +0330

    change

--- src/main/java/shop/fh/food/address/AddressController.java
+++ src/main/java/shop/fh/food/address/AddressController.java
@@ -1,9 +1,10 @@
 package shop.fh.food.address;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import shop.fh.food.foodcategory.FoodCategoryEntity;
+
+import java.util.Optional;
 
 
 @Controller
@@ -11,8 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("address")
 public class AddressController {
 
+    private AddressRepository addressRepository;
+
+    public AddressController(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
     @GetMapping("title")
     public String getApi(){
         return "hello address";
+    }
+
+    @PostMapping("add")
+    public void addAddress(@RequestBody AddressEntity address){
+        addressRepository.save(address);
+    }
+    @GetMapping("find")
+    public Optional<AddressEntity> findAddressById(@RequestParam String id){
+        return addressRepository.findById(id);
+    }
+
+    @GetMapping("find-by-category")
+    public Optional<AddressEntity> findAddressByCategory(@RequestParam FoodCategoryEntity category){
+        return null;// addressRepository.find(id);
     }
 }
